2974db80c8a3cdc08a90301b56399670742f96ed
Eduardo Gondo <egondo@gmail.com>
2024-08-21 22:49:57 -0300

    Processamento do arquivo de precos

--- ArquivoObjeto/src/br/com/fiap/app/ConversaoArquivoLista.java
+++ ArquivoObjeto/src/br/com/fiap/app/ConversaoArquivoLista.java
@@ -1,6 +1,8 @@
 package br.com.fiap.app;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import br.com.fiap.model.Registro;
 import br.com.fiap.negocio.ProdutoBO;
@@ -13,8 +15,24 @@ public class ConversaoArquivoLista {
 		String arq = "/Users/eduardogondo/Downloads/precos.csv";
 		try {
 			List<Registro> lista = neg.leArquivoPrecos(arq);
+//			for(Registro reg : lista) {
+//				System.out.println(reg.getLocal() + " " + reg.getProduto());
+//			}
+			
+			//criando objeto Map
+			Map<String, Integer> map = new HashMap<String, Integer>();
 			for(Registro reg : lista) {
-				System.out.println(reg.getLocal() + " " + reg.getProduto());
+				if (map.containsKey(reg.getProduto())) {
+					int num = map.get(reg.getProduto());
+					num++;
+					map.put(reg.getProduto(), num);
+				}
+				else {
+					map.put(reg.getProduto(), 1);
+				}
+			}
+			for(String key : map.keySet()) {
+				System.out.println(key + "=>" + map.get(key));
 			}
 			
 		} catch (Exception e) {
