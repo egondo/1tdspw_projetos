0b84fb4407dcd1eb726dd43b43383a6ee73844b0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-05 22:56:18 -0300

    Novos metodos do banco

--- persistencia/src/main/java/br/com/fiap/persistencia/dao/PacienteDao.java
+++ persistencia/src/main/java/br/com/fiap/persistencia/dao/PacienteDao.java
@@ -6,7 +6,10 @@ import br.com.fiap.persistencia.util.Conexao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PacienteDao {
 
@@ -26,21 +29,82 @@ public class PacienteDao {
             pstmt.setDate(6, Date.valueOf(pac.getNascimento()));
             pstmt.executeUpdate();
         }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
+    public Paciente recupera(long id) throws Exception {
+        Conexao c = new Conexao();
+        String sql = "select id, nome, nascimento, email, cpf, telefone, responsavel " +
+                "from t_paciente where id = ?";
+        Paciente paciente = null;
+        try(Connection con = c.getConexao();
+            PreparedStatement pstmt = con.prepareStatement(sql);) {
+
+            pstmt.setLong(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                paciente = new Paciente();
+                paciente.setId(id);
+                paciente.setCpf(rs.getLong("cpf"));
+                paciente.setNome(rs.getString("nome"));
+                paciente.setEmail(rs.getString("email"));
+                paciente.setTelefone(rs.getString("telefone"));
+                paciente.setResponsavel(rs.getString("responsavel"));
+                paciente.setNascimento(rs.getDate("nascimento").toLocalDate());
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return paciente;
+    }
+
+    public List<Paciente> recupera() throws Exception {
+        List<Paciente> lista = new ArrayList<>();
+        Conexao c = new Conexao();
+        String sql = "select id, nome, nascimento, email, cpf, telefone, responsavel " +
+                "from t_paciente order by nome";
+        Paciente paciente = null;
+        try (Connection con = c.getConexao();
+             PreparedStatement pstmt = con.prepareStatement(sql);) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                paciente = new Paciente();
+                paciente.setId(rs.getLong("id"));
+                paciente.setCpf(rs.getLong("cpf"));
+                paciente.setNome(rs.getString("nome"));
+                paciente.setEmail(rs.getString("email"));
+                paciente.setTelefone(rs.getString("telefone"));
+                paciente.setResponsavel(rs.getString("responsavel"));
+                paciente.setNascimento(rs.getDate("nascimento").toLocalDate());
+                lista.add(paciente);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
     public static void main(String[] args) {
         Paciente p = new Paciente();
         p.setNascimento(LocalDate.of(2004, 8, 23));
         p.setCpf(234938928);
-        p.setNome("Abel Ferreira");
-        p.setEmail("abel@fiap.com.br");
+        p.setNome("Eduardo Gondo");
+        p.setEmail("profeduardo@fiap.com.br");
         p.setTelefone("234230494");
         p.setResponsavel(null);
 
         PacienteDao pdao = new PacienteDao();
         try {
             pdao.inclui(p);
+            Paciente teste = pdao.recupera(1);
+            System.out.println(teste.getNome() + " " + teste.getEmail());
+
+            List<Paciente> lista = pdao.recupera();
+            for(Paciente pac : lista) {
+                System.out.println(pac.getNome() + " " + pac.getNascimento());
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
